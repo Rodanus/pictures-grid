c0bbf092c4bdc2de953e2cf60d6df44a65ea7ad2
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2020-06-07 20:48:29 +0300

    Display components

--- src/components/PhotosList.jsx
+++ src/components/PhotosList.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Photo from "./Photo";
 
 export default function PhotosList() {
-  return <h2>this is inside the PhotosList component</h2>;
+  return (
+    <div className="photos">
+      <h2>this is inside the PhotosList component</h2>
+      <Photo />
+    </div>
+  );
 }
