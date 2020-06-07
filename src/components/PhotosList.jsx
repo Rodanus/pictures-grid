50b04bbaa69617651b1bf1fd040aa717fb8b6464
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2020-06-07 21:10:43 +0300

    Add photos list code

--- src/components/PhotosList.jsx
+++ src/components/PhotosList.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Photo from "./Photo";
 
-export default function PhotosList() {
+export default function PhotosList({ photos }) {
   return (
     <div className="photos">
-      <h2>this is inside the PhotosList component</h2>
-      <Photo />
+      {photos.length > 0 ? (
+        //   Todo: make sure that you pass the url of the photo instead of the whole photo
+        photos.map(photo => <Photo photo={photo} />)
+      ) : (
+        <h2>Search for something to see the photos!</h2>
+      )}
     </div>
   );
 }
