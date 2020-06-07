b39c4d6ba4927205da078e8d4793721f5ddf057f
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2020-06-07 21:04:25 +0300

    Add app component code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Search from "./components/Search";
 import PhotosList from "./components/PhotosList";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+
   return (
     <div className="App">
-      <Search />
-      <PhotosList />
+      <Search setPhotos={setPhotos} />
+      <PhotosList photos={photos} />
     </div>
   );
 }
