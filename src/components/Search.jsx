984f902f5191bdd5f928b640f5b8217924a5b7ec
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2020-06-09 19:00:52 +0300

    add: Ability to search for photos
    This doesn't include showing the photo

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
+import axios from "axios";
+import debounce from "lodash/debounce";
 
-export default function Search() {
+export default function Search({ setPhotos }) {
   const [value, setValue] = useState("");
+
+  const getPhotos = value => {
+    // To get rid of extra spaces
+    const trimedValue = value.trim();
+
+    // Checks whether the value is empty or not
+    if (trimedValue) {
+      console.log("Calling the api...");
+      axios
+        .get(
+          `https://api.unsplash.com/search/photos?page=1&per_page=4&query=${trimedValue}&client_id=${process.env.REACT_APP_CLIENTID}`
+        )
+        .then(res => {
+          setPhotos(res.data.results);
+          console.log(res.data.results);
+        });
+    }
+  };
+
+  // https://medium.com/@rajeshnaroth/using-throttle-and-debounce-in-a-react-function-component-5489fc3461b3
+  const debouncedGetPhotos = useCallback(
+    debounce(value => getPhotos(value), 500),
+    []
+  );
+
+  const onChange = e => {
+    setValue(e.target.value);
+    debouncedGetPhotos(e.target.value);
+  };
 
   return (
     <input
       type="text"
       value={value}
       placeholder="Search"
-      onChange={e => setValue(e.target.value)}
+      onChange={e => onChange(e)}
     />
   );
 }
