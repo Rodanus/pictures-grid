f99969f66f41e92eb330a50bcd6a9ce4005e611c
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2020-07-14 23:17:54 +0300

    Display images with masonry

--- src/components/PhotosList.jsx
+++ src/components/PhotosList.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Photo from "./Photo";
 import "./PhotosList.css";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 export default function PhotosList({ photos }) {
   return (
     <div className="photos">
       {photos.length > 0 ? (
-        photos.map(photo => <Photo key={photo.id} photo={photo} />)
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 600: 2, 900: 3 }}>
+          <Masonry gutter="10px">
+            {photos.map((photo, id) => (
+              <img
+                key={id}
+                src={photo.urls.small}
+                alt={photo.alt_description}
+                style={{ width: "100%", display: "block" }}
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
       ) : (
         <h2>Search for something to see the photos!</h2>
       )}
