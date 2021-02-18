b946e8361a772c55315eecee8ca525eeb6b695f8
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2021-02-18 17:44:29 +0300

    Design photos list section

--- src/components/PhotosList.jsx
+++ src/components/PhotosList.jsx
@@ -5,16 +5,17 @@ import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 export default function PhotosList({ photos }) {
   return (
-    <div className="photos">
+    <section className="photos-list-section">
+      <h2 className="photos-list-sec-title">For you to check:</h2>
       {photos.length > 0 ? (
         <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 600: 2, 900: 3 }}>
-          <Masonry gutter="10px">
+          <Masonry gutter="20px">
             {photos.map((photo, id) => (
               <Photo key={id} photo={photo} />
             ))}
           </Masonry>
         </ResponsiveMasonry>
       ) : null}
-    </div>
+    </section>
   );
 }
