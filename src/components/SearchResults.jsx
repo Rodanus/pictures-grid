dd0443c557903ef7a8dc0cc9bd9ea849ab6c6b33
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2021-06-07 14:32:23 +0300

    add homepage link

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,26 +2,29 @@ import React, { useEffect, useState } from "react";
 import PhotosList from "./PhotosList";
 import Search from "./Search";
 import "./SearchResults.css";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function SearchResults(props) {
   const [photos, setPhotos] = useState([]);
   const currentLocation = useLocation();
 
   // Update whenever the url changes.
-  useEffect(() => {
-    const fetchingData = async () => {
-      const searchResults = await fetch(
-        `https://api.unsplash.com/search/photos?page=1&per_page=30&query=${props.match.params.query}&client_id=${process.env.REACT_APP_CLIENTID}`
-      )
-        .then(res => res.json())
-        .then(res => setPhotos(res.results));
-    };
-    fetchingData();
-  }, [currentLocation]);
+  // useEffect(() => {
+  //   const fetchingData = async () => {
+  //     const searchResults = await fetch(
+  //       `https://api.unsplash.com/search/photos?page=1&per_page=30&query=${props.match.params.query}&client_id=${process.env.REACT_APP_CLIENTID}`
+  //     )
+  //       .then(res => res.json())
+  //       .then(res => setPhotos(res.results));
+  //   };
+  //   fetchingData();
+  // }, [currentLocation]);
 
   return (
     <div className="search-results-container">
+      <Link to="/" className="home-link">
+        Home
+      </Link>
       <Search history={props.history} />
       <section className="search-results">
         <PhotosList photos={photos} title={`Your Search Results:`} />
