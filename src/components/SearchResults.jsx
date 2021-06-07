fc6ccc3263402dd01c368c2da0a6f63823186cb9
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2021-06-07 18:55:23 +0300

    display a message when no results are found

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -6,19 +6,33 @@ import { Link, useLocation } from "react-router-dom";
 
 export default function SearchResults(props) {
   const [photos, setPhotos] = useState([]);
+  const [result, setResult] = useState({
+    foundResults: false,
+    wordSearchedFor: ""
+  });
   const currentLocation = useLocation();
 
   // Update whenever the url changes.
-  // useEffect(() => {
-  //   const fetchingData = async () => {
-  //     const searchResults = await fetch(
-  //       `https://api.unsplash.com/search/photos?page=1&per_page=30&query=${props.match.params.query}&client_id=${process.env.REACT_APP_CLIENTID}`
-  //     )
-  //       .then(res => res.json())
-  //       .then(res => setPhotos(res.results));
-  //   };
-  //   fetchingData();
-  // }, [currentLocation]);
+  useEffect(() => {
+    const fetchingData = async () => {
+      const searchResults = await fetch(
+        `https://api.unsplash.com/search/photos?page=1&per_page=30&query=${props.match.params.query}&client_id=${process.env.REACT_APP_CLIENTID}`
+      )
+        .then(res => res.json())
+        .then(res => {
+          const results = res.results;
+
+          if (results.length > 0) {
+            setResult({ foundResults: true });
+            return setPhotos(results);
+          }
+
+          // when no results found for the searched word
+          setResult({ foundResults: false, word: props.match.params.query });
+        });
+    };
+    fetchingData();
+  }, [currentLocation]);
 
   return (
     <div className="search-results-container">
@@ -27,7 +41,13 @@ export default function SearchResults(props) {
       </Link>
       <Search history={props.history} />
       <section className="search-results">
-        <PhotosList photos={photos} title={`Your Search Results:`} />
+        {result.foundResults ? (
+          <PhotosList photos={photos} title={`Your Search Results:`} />
+        ) : (
+          <h2 className="no-results-found-text">
+            No Results Found For: {result.word}
+          </h2>
+        )}
       </section>
     </div>
   );
