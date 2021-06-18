9677dbc174655a089b76962fe057eb908b731ba0
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2021-06-18 23:15:19 +0300

    Close photo model when close button is clicked

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import "./Overlay.css";
+import closeIcon from "../svg/close-icon.svg";
 
 export default function Overlay({ toggleViewPhoto }) {
   const handleOnClick = () => {
     toggleViewPhoto();
   };
 
-  return <div className="overlay" onClick={handleOnClick}></div>;
+  return (
+    <div className="overlay" onClick={handleOnClick}>
+      <button className="close-button" title="Close">
+        <img src={closeIcon} alt="Close icon" className="close-icon" />
+      </button>
+    </div>
+  );
 }
